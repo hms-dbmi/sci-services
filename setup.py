73419ae2b8d9c53ae0b8b4de0af3e6d3664fbd04
mtmcduffie <mcduffie.michael@gmail.com>
2018-02-23 16:05:09 -0500

    Initial Commit.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='sciservices',
+    version='1.0',
+    packages=['sciservices',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
